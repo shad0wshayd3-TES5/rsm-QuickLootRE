107e89df3fb4c09b2bbfbbc80dffbd6cfcd9d2c3
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-01-01 20:20:09 -0700

    build fixes

--- src/c++/PCH.h
+++ src/c++/PCH.h
@@ -1,56 +1,24 @@
 #pragma once
+#pragma warning(disable : 4702)
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
-#include <algorithm>
-#include <array>
-#include <atomic>
-#include <bitset>
-#include <cassert>
-#include <cstddef>
-#include <cstdint>
-#include <cstdlib>
-#include <functional>
-#include <initializer_list>
-#include <limits>
-#include <memory>
-#include <mutex>
-#include <optional>
-#include <set>
-#include <span>
-#include <string>
-#include <string_view>
-#include <thread>
-#include <tuple>
-#include <type_traits>
-#include <typeinfo>
-#include <utility>
-#include <variant>
-#include <vector>
-
-#pragma warning(push)
 #include <boost/regex.hpp>
 #include <frozen/map.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
-#ifdef NDEBUG
-#	include <spdlog/sinks/basic_file_sink.h>
-#else
-#	include <spdlog/sinks/msvc_sink.h>
-#endif
-#pragma warning(pop)
-
 #include "AutoTOML.hpp"
 
-#undef GetObject
-#undef max
-#undef min
+#define DLLEXPORT __declspec(dllexport)
+
+using namespace std::literals;
 
 namespace logger = SKSE::log;
 namespace WinAPI = SKSE::WinAPI;
-
-using namespace std::literals;
 
 namespace stl
 {
@@ -59,8 +27,12 @@ namespace stl
 	constexpr std::string_view safe_string(const char* a_str) { return a_str ? a_str : ""sv; }
 }
 
-#define DLLEXPORT __declspec(dllexport)
+#ifdef SKYRIM_AE
+# define OFFSET(se, ae) ae
+#else
+# define OFFSET(se, ae) se
+#endif
 
-#include "Plugin.h"
+#include "Version.h"
 
 #include "Settings.h"
--- src/c++/Settings.h
+++ src/c++/Settings.h
@@ -8,7 +8,7 @@ struct Settings
 	static void load()
 	{
 		try {
-			const auto table = toml::parse_file("Data/SKSE/Plugins/QuickLootRE.toml"s);
+			const auto table = toml::parse_file("Data/SKSE/Plugins/QuickLootRE/config.toml"s);
 			for (const auto& setting : ISetting::get_settings()) {
 				setting->load(table);
 			}
@@ -18,7 +18,7 @@ struct Settings
 				<< "Error parsing file \'" << *e.source().path << "\':\n"
 				<< '\t' << e.description() << '\n'
 				<< "\t\t(" << e.source().begin << ')';
-			logger::error(ss.str());
+			logger::error(FMT_STRING("{:s}"sv), ss.str());
 			throw std::runtime_error("failed to load settings"s);
 		}
 	}
