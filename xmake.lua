20c51cf68bbbfc99571e43d9f8be793c16f7eeef
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-05-19 01:35:10 -0600

    refactor: project update

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,8 @@
--- set xmake version
-set_xmakever("2.9.4")
+-- set minimum xmake version
+set_xmakever("2.8.2")
 
--- include local folders
-includes("extern/commonlibsse")
+-- includes
+includes("lib/commonlibsse")
 
 -- set project
 set_project("QuickLootRE")
@@ -10,26 +10,24 @@ set_version("2.17.0")
 set_license("MIT")
 
 -- set defaults
-set_arch("x64")
 set_languages("c++23")
-set_optimize("faster")
-set_warnings("allextra", "error")
-set_defaultmode("releasedbg")
-
--- enable lto
-set_policy("build.optimization.lto", true)
+set_warnings("allextra")
 
 -- add rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- add config
+-- set policies
+set_policy("build.optimization.lto", true)
+set_policy("package.requires_lock", true)
+
+-- set configs
 set_config("rex_toml", true)
 set_config("skyrim_ae", true)
 
--- setup targets
+-- targets
 target("QuickLootRE")
-    -- bind local dependencies
+    -- add dependencies to target
     add_deps("commonlibsse")
 
     -- add commonlibsse plugin
@@ -38,11 +36,11 @@ target("QuickLootRE")
         author = "ryan-rsm-mckenzie"
     })
 
-    -- add source files
+    -- add src files
     add_files("src/**.cpp")
     add_headerfiles("src/**.h")
     add_includedirs("src")
-    set_pcxxheader("src/PCH.h")
+    set_pcxxheader("src/pch.h")
 
     -- add extra files
     add_extrafiles(".clang-format")
