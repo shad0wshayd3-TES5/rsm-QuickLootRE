cd9892c4c64eb8c449a3567b0f807e4c6b81e3cc
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-20 20:54:11 -0600

    refactor: project update

--- xmake.lua
+++ xmake.lua
@@ -1,36 +1,22 @@
--- set minimum xmake version
-set_xmakever("2.8.2")
-
--- includes
+-- include subprojects
 includes("lib/commonlibsse")
 
--- set project
+-- set project constants
 set_project("QuickLootRE")
-set_version("2.17.0")
+set_version("3.0.0")
 set_license("MIT")
-
--- set defaults
 set_languages("c++23")
 set_warnings("allextra")
 
--- add rules
+-- add common rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- set policies
-set_policy("build.optimization.lto", true)
-set_policy("package.requires_lock", true)
-
 -- set configs
-set_config("rex_toml", true)
-set_config("skyrim_ae", true)
+set_config("commonlib_toml", true)
 
--- targets
+-- define targets
 target("QuickLootRE")
-    -- add dependencies to target
-    add_deps("commonlibsse")
-
-    -- add commonlibsse plugin
     add_rules("commonlibsse.plugin", {
         name = "QuickLootRE",
         author = "ryan-rsm-mckenzie"
